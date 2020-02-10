ffe168b01e2a9fc3187e9941d283d003d978151b
GuiTadeu <guitadeu.py@gmail.com>
2020-02-10 17:46:26 -0300

    Refatorando classes

--- src/main/java/com/getout/validation/ApiErrors.java
+++ src/main/java/com/getout/validation/ApiErrors.java
@@ -2,6 +2,7 @@ package com.getout.validation;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 
 import java.util.List;
 
@@ -9,8 +10,8 @@ import static java.util.stream.Collectors.toList;
 
 public class FieldErrors {
 
-    public static List<String> convertErrorsToJson(BindingResult result) {
-        return result.getAllErrors()
+    public static List<String> convertErrorsToJson(Errors errors) {
+        return errors.getAllErrors()
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(toList());
